7f680d8c5015ddefb07b9cfe87f68165e83ff832
Shengwei Lyu <samlyu@users.noreply.github.com>
2019-08-19 17:41:49 +0800

    Learning perl (08/19/2019)

---
+++ Tutorials/09_sub.pl
@@ -0,0 +1,50 @@
+use strict;
+use warnings;
+
+# sub 
+# call function:
+# &subname; sub subname{}
+
+my $result = &add(1,2);
+$result = $result + 1;
+print "result = $result\n";
+my $data = $result + &add(1,3);
+print "data = $data\n";
+sub add
+{
+	my @array = @_;
+	my $val = 0;
+	foreach my $item(@array)
+	{
+		$val = $val + $item;
+	}
+	return $val;	# all ignored after return
+}
+
+# call without &: predeclare or have () after sub
+# call without (): predeclare
+# do add(1,2);
+# parameter passing:
+sub add1
+{
+	my ($num1, $num2, $num3) = @_;	# need to follow the order
+	my $val = $num1 + $num2 + $num3;
+	return $val;
+}
+
+# use local in main, can be seen in local
+# use reference:
+sub sum
+{
+	my ($aref) = @_;
+	my $total = 0;
+	foreach(@$aref)
+	{
+		$total += $_;
+	}
+	return $total;
+}
+my @a = (1,2,3);
+my $resulti = sum (\@a);
+print "sum = $resulti\n";
+
---
+++ Tutorials/10_file.pl
@@ -0,0 +1,61 @@
+use strict;
+use warnings;
+
+# STDIN: input, STDOUT: output, STDERR: error
+print "input data: \n";
+my $data = <STDIN>;
+print STDOUT "The data is $data";
+
+# open(file, path);
+if (open (MYFILE, "10_file.pl"))
+{
+	print "success\n";
+}
+else
+{
+	print "cannot open file\n";
+	exit 1;
+}
+# open(file, path) or die;
+# open (MYFILE, "11_file.pl") or die "not exists: $!\n";
+# warn: print warning, not terminate program
+if(!open (MYFILE, "12_file.pl"))
+{
+	warn "cannot open file: $!";
+}print "another command here\n";
+
+# <read one line>
+open (MYFILE, "test.txt") or die "cannnot open test file:$!\n";
+while(my $line = <MYFILE>)
+{
+	print "$line";
+}print "\n";
+close MYFILE;
+
+open (MYFILE, "test.txt") or die "cannnot open test file:$!\n";
+my @array = <MYFILE>;
+print @array;
+print "\n";
+close MYFILE;
+
+open (MYFILE, ">>test.txt");	# add >>, overwrite >
+print MYFILE "\nline3\n";
+close MYFILE;
+
+open (MYFILE, "test.txt");
+my @array = <MYFILE>;
+print @array;
+close MYFILE;
+
+# file test
+# -r 'file': return if readable
+# -w $file: return if writeable
+# -e 'file': return if exist
+# -z 'file': return if exist and empty
+# -s 'file': return size (num of chars) if exist
+# -f 'a.txt': return if is a normal file
+# -d '/tmp': return if is a directory
+# -T 'data': return if data is a text file
+# -B 'data': return is data is a bin file
+# -M 'data': return num of days after 'data' was modified
+
